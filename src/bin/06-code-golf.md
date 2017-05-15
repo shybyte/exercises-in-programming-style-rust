a343e47e525616bcd819bfa5250bbfd604a4095a
shybyte <ein@volloeko.de>
2017-05-15 07:19:53 +0200

    Initial commit

---
+++ src/bin/06-code-golf.md
@@ -0,0 +1,6 @@
+# Style #6 - Code Golf
+
+Constraints:
+
+- As few code as possible
+- Clever use of (standard) libraries
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("The real code is in the bin folder. Example: \"cargo run --bin 06-code-golf test-data/input/simple.txt\"");
+}
