cb8aa5ad7b68a5dc0ab6b6c8ae05546eca0448ca
shybyte <ein@volloeko.de>
2017-05-19 14:57:46 +0200

    Measure maximum memory

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ for exe in $(find . -regex "./target/release/[0-9]+.*" ); do
   if [ -x $exe ]; then
     for input in test-data/input/*.txt ; do
       echo -n "Testing  $exe with input $input ... "
-      /usr/bin/time -f "%Us" -o time.tmp.txt $exe $input >output.tmp.txt
+      /usr/bin/time -f "%Us %Mkb" -o time.tmp.txt $exe $input >output.tmp.txt
       diff output.tmp.txt ${input/input/output}
       result=$?
       if [ $result -ne 0 ]; then
